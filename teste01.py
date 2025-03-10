d2da9a72aeb940b7e51280958387e0cd8e73d0fa
Leonardo Carrera da Silva <59487403+LeonardoCarreraSilva@users.noreply.github.com>
2025-03-10 19:03:51 -0300

    add meu primeiro script python.

---
+++ teste01.py
@@ -0,0 +1,4 @@
+nome = input('Qual o seu nome? ')
+idade = input('Qual a sua idade? ')
+peso = input('Qual o seu peso? ')
+print('Seu nome é', nome, 'você tem', idade, 'anos e pesa', peso, 'quilos.')
