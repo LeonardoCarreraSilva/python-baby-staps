70baba6d0cede4ec6fd50335f162b0d01e90bc16
Leonardo Carrera da Silva <59487403+LeonardoCarreraSilva@users.noreply.github.com>
2025-03-10 19:15:25 -0300

    add desafio iniciante.

---
+++ saldacao.py
@@ -0,0 +1,14 @@
+# desafio 1
+# nome = input('Qual o seu nome? ')
+# print('Ola ' + nome + ', é um praser te conhecer!')
+# desafio 2
+# dia = input('Dia: ')
+# mes = input('Mes: ')
+# ano = input('Ano: ')
+# print('Você nasceu no dia ' , dia , ' de ', mes, ' de ', ano, '. Correto?')
+
+# desafio 3
+x = input('Primeiro numero: ')
+y = input('Segundo numero: ')
+
+print('A soma é: ', int(x) + int(y))
